2a458f26467010b5911c2859b8a47794c77f3a70
Zaidi <zaidiahsan.r@gmail.com>
2022-09-09 18:47:32 -0500

    add employee to database

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const Department = require('./classes/js/Department');
 const Role = require('./classes/js/Role');
 const { appendFile } = require("fs");
 const { default: prompt } = require("inquirer/lib/ui/prompt");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 //if connection is made run app
 connection.connect((error) => {
@@ -30,7 +31,7 @@ function promptUser() {
                 "ADD A DEPARTMENT",
                 "ADD A ROLE",
                 "ADD AN EMPLOYEE",
-                "UPDATE AN EMPLOYEES ROLE",
+                "UPDATE AN EMPLOYEE'S ROLE",
                 "UPDATE EMPLOYEE'S MANAGER",
                 "DELETE DEPARTMENT",
                 "DELETE ROLE",
@@ -57,6 +58,169 @@ function promptUser() {
         }
         else if (question.choice === "VIEW EMPLOYEES BY MANAGER") {
             let emp = new Employee(1, "a", "b", 1, 2);
+            emp.ViewEmployeeByMgr(connection);
+            promptUser();
         }
+        else if (question.choice === "VIEW EMPLOYEES BY DEPARTMENT") {
+            let emp = new Employee(1, "a", "b", 2, 1);
+            emp.ViewEmployeeByDpt(connection);
+            promptUser();
+        }
+        else if (question.choice === "ADD A DEPARTMENT") {
+            addDept();
+        }
+        else if (question.choice === "ADD A ROLE") {
+            addRole();
+        }
+        else if (question.choice === "ADD AN EMPLOYEE") {
+            addEmp();
+        }
+        else if (question.choice === "UPDATE AN EMPLOYEE'S ROLE") {
+            updateRole();
+        }
+        else if (question.choice === "UPDATE EMPLOYEE'S MANAGER") {
+            updateMgr();
+        }
+        else if (question.choice === "DELETE ROLE") {
+            removeRole();
+        }
+        else if (question.choice === "DELETE AN EMPLOYEE") {
+            removeEmployee();
+        }
+        else if (question.choice === "DELETE DEPARTMENT") {
+            deleteDept();
+        }
+        else if (question.choice === "VIEW BUDGET OF DEPARTMENT") {
+            BUDGET();
+        }
+        else if (question.choice === "EXIT") {
+            process.exit();
+        }
+        console.log(``);
     })
 }
+
+//Function to Add a department
+const addDept = () => {
+    inquirer.prompt({
+        type: 'input',
+        name: 'deptName',
+        message: "Enter new department name"
+    }).then(Response => {
+        let dept = new Department(Response.deptName);
+        dept.AddDepartment(connection, dept.getDeptName());
+        console.log('Successfully added new Department!');
+        promptUser();
+    })
+}
+
+//Function to Delete a department
+const deleteDept = () => {
+    var depts = [];
+    let sqlQuery = `Select name from Department`;
+    connection.query(sqlQuery, (err, res) => {
+        if (err) throw err;
+        for(let i = 0; i < res.length; i++) {
+            depts.push(res[i].name);
+        }
+        inquirer.prompt({
+            type: 'list',
+            name: 'Departments',
+            message: 'Choose the department you would like to delete',
+            choices: depts
+        }).then(response => {
+            for (let i = 0; i < depts.length; i++) {
+                if (response.Departments == depts[i]) {
+                    let dept = new Department(response.Departments);
+                    dept. DeleteDepartment(connection, dept.getDeptName())
+                    break;
+                }
+            }
+            console.log('New department was successfully Deleted!');
+            promptUser();
+        })
+    });
+}
+
+//Function to Add An employee to the Database
+const addEmp = () => {
+    const emp = [];
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Enter Employees first name',
+            validate: firstName => {
+                if (firstName) {
+                    return true;
+                } else {
+                    console.log('Please enter a first name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Enter Employees last name',
+            validate: lastName => {
+                if (lastName) {
+                    return true;
+                } else {
+                    console.log('Please enter a last name!');
+                    return false;
+                }
+            }
+        }
+    ]).then(response => {
+        emp.push(response.firstName);
+        emp.push(response.lastName);
+        var roles = [];
+        let sqlQuery = `Select title from role`;
+        connection.query(sqlQuery, (err, res) => {
+            if (err) throw err;
+            for (let i = 0; i < res.length; i++) {
+                roles.push(res[i].title);
+            }
+            inquirer.prompt({
+                type: 'list',
+                name: 'role',
+                message: 'Choose the role you would like for this Employee',
+                choices: roles
+            }).then(response => {
+                let sqlQuery = `Select id from role where title="` + response.role + `"`;
+                connection.query(sqlQuery, (err, res) => {
+                    if (err) throw err;
+                    emp.push(res[0].id)
+                })
+                var mgr = [];
+                let sql = `Select first_name as MANAGER from employee`;
+                connection.query(sqlQuery, (err, res) => {
+                    if (err) throw err;
+                    for (let i = 0; i < res.length; i++) {
+                        mgr.push(res[i].MANAGER);
+                    }
+                    inquirer.prompt({
+                        type: 'list',
+                        name: 'mgr',
+                        message: 'Choose the Manager you would like for this employee',
+                        choices: mgr
+                    }).then(response => {
+                        let sqlQuery = `Select id as mgrID from employee where first_name="` + response.mgr + `"`;
+                        connection.query(sqlQuery, (err, res) => {
+                            if (err) throw err;
+                            emp.push(res[0].mgrID)
+                            console.log(emp)
+                            let e = new Employee(1, emp[0], emp[1], emp[2]. emp[3]);
+                            console.log(e)
+                            e.addEmployee(connection, e.getFirstName(), e.getLastName(), e.getRid(), e.getMgrId());
+                            console.log('New employee successfully added!');
+                            promptUser();
+                        })
+                    })
+                });
+            });
+        });
+    });
+}
+
